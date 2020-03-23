b1a3e781e931c6e35544d1abe5f08979f3378894
Dima <mister.mitya99@gmail.com>
2020-03-23 14:56:36 +0300

    added routing and pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Redirect
-} from 'react-router-dom';
+} from "react-router-dom";
+import { connect } from "react-redux";
+import "materialize-css";
 
-import HomePage from './container/HomeContainer';
-import UserProfilePage from './container/ProfileContainer';
+import HomePage from "./page/HomePage";
+import UserProfilePage from "./page/UserProfilePage";
+import { thunkUsersFecthData } from "./serve/index";
 
+const App = ({ loadUsers }) => {
+    useEffect(() => {
+        loadUsers();
+    }, [loadUsers]);
 
-export const App = ()  => (
-    <Router>
-        <div className="container">
-            <Switch>
-                <Route path="/home" exact>
-                    <HomePage />
-                </Route>
-                <Route path="/profile/:id">
-                    <UserProfilePage />
-                </Route>
-                <Redirect to="/home" />
-            </Switch>
-        </div>
-    </Router>
-);
+    return (
+        <Router>
+            <div className="container">
+                <Switch>
+                    <Route path="/home" exact>
+                        <HomePage />
+                    </Route>
+                    <Route path="/profile/:id/:editing?">
+                        <UserProfilePage />
+                    </Route>
+                    <Redirect to="/home" />
+                </Switch>
+            </div>
+        </Router>
+    );
+};
+
+App.propTypes = {
+    loadUsers: PropTypes.func.isRequired
+};
+
+export default connect(null, dispatch => ({
+    loadUsers: () => dispatch(thunkUsersFecthData())
+}))(App);
--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import UserList from "../container/UserListContainer";
 
-export const HomePage = () => (
-    <div>
-        <h1>Home Page</h1>
-    </div>
-);
+import SeacrhInputContainer from "../container/SeacrhInputContainer";
+import { UserAddForm } from "../components/user-add-form/UserAddForm";
+
+const HomePage = ({ error }) =>
+    error ? (
+        <h2>We apologize, the site is not working right now </h2>
+    ) : (
+        <div>
+            <SeacrhInputContainer />
+            <UserAddForm />
+            <UserList />
+        </div>
+    );
+
+HomePage.propTypes = {
+    error: PropTypes.bool.isRequired
+};
+
+export default connect(store => ({
+    error: store.error
+}))(HomePage);
